775cbf9c1887dba0f4235ba5190abc1727f9ab7c
BrunoMartins <BrunoMartins11>
2018-02-27 12:39:23 +0000

    criadas classes contribuinte e empresa(incompleta)

---
+++ Trabalho/src/Contribuinte.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Contribuinte {
+    private int NIF;
+    private String email;
+    private String nome;
+    private String morada;
+    private String password;
+    private int dependetes;
+    private ArrayList<String> NIFdependentes;
+    private float coeficiente;
+    private Map<Integer,ArrayList<String>> Atvidadesposs;
+
+
+}
---
+++ Trabalho/src/Empresa.java
@@ -0,0 +1,8 @@
+public class Empresa {
+    private int NIF;
+    private String email;
+    private String nome;
+    private String morada;
+    private String password;
+    // falta as especifidades das empresas
+}
