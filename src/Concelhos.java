d3b24b91442fcf810eead21911de0abf4232f833
BrunoMartins11 <brunocmartins@live.com.pt>
2018-05-24 16:50:39 +0100

    classe dos concelhos

---
+++ src/Concelhos.java
@@ -0,0 +1,34 @@
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Concelhos {
+    /**
+     * Lista que contém os concelhos do interior
+     */
+    private static final List<String> concelhos = new ArrayList<>();
+    static {
+        concelhos.add("Vila Real");
+        concelhos.add("Bragança");
+        concelhos.add("Vila Pouca de Aguiar");
+        concelhos.add("Alijó");
+        concelhos.add("Murça");
+        concelhos.add("Boticas");
+        concelhos.add("Valpaços");
+        concelhos.add("Mirandela");
+        concelhos.add("Sabrosa");
+        concelhos.add("Chaves");
+        concelhos.add("Peso da Régua");
+    }
+
+    /**
+     * Verifica se o concelhoe do interior
+     * @param c concelho
+     * @return boolean
+     */
+    public boolean contemConcelho(String c){
+        return concelhos.contains(c);
+    }
+
+
+}
